53367eb6f97add2002d00c77d0de96552792741a
XujuanChen <xchen022@citymail.cuny.edu>
2022-08-09 22:29:48 -0400

    remove cache

---
+++ 04-accordion/src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import './index.css';
+import Question from './Question'
+
+function App() {
+
+  return (
+    <main>
+      <div className="title">
+        <h2>Questions</h2>
+        <div className="underline"></div>
+      </div>
+      <Question />
+    </main>
+  );
+}
+
+export default App;
---
+++ 04-accordion/src/Question.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import data from './data';
+import {AiFillPlusCircle, AiFillMinusCircle} from 'react-icons/ai'
+
+const Question = () => {
+  const [items, setItems] = useState(data);
+  const [text, setText] = useState(false);
+
+  return (
+    <section className='section'>
+
+      {
+        items.map((item)=>{
+          const {id, title, info} = item;
+
+          const handleClick = (e) => {
+            const id = e.target.id;
+            setText(!text)
+          }
+          return(
+            <div className='question-container' key={id}>
+              <div className='question'>
+                <h4>{title}</h4>
+                <button className='btn' onClick={(e) =>handleClick(e, e.target.id)}>
+                  { text?<AiFillMinusCircle/> : <AiFillPlusCircle/> }
+                </button>
+              </div>
+              <p className='answer'> { text && info }</p>
+            </div>
+          )
+        })
+      }
+    </section>
+  )
+}
+
+export default Question
